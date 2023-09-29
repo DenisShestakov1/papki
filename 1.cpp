a5d6fcff088f105e322539f1824cbdc804b76150
r9jgfja <112956409+DenisShestakov1@users.noreply.github.com>
2023-09-29 13:00:41 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,69 +1,56 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
+#include <iostream> 
+#include <fstream> 
+#include <string>   
+void generateNames(std::string uniqueChars, int length, std::string& name, std::ofstream& logFile, int& fileCount) {
+    // Функция для генерации имен файлов и папок.
 
-void generateNames(std::string uniqueChars, int length, std::vector<std::string>& names) {
-    if (length == 0) {
-        return;
-    }
-    for (char c : uniqueChars) {
-        std::string name(1, c);
-        names.push_back(name);
+    for (char c : uniqueChars) { // Для каждого символа 'c' в строке 'uniqueChars' выполняем следующий блок кода.
+        std::string newName = name + c; // Создаем новое имя, добавляя 'c' к текущему 'name'.
 
-        if (length > 1) {
-            std::vector<std::string> subNames;
-            generateNames(uniqueChars, length - 1, subNames);
+        if (length == 1) { // Если длина 'length' равна 1, то выполняем следующий блок кода (генерация имен файлов).
+        
+                std::cout << "Создан файл " << newName << std::endl; // Выводим сообщение о создании файла в консоль.
+                logFile << "Создан файл " << newName << std::endl;   // Записываем сообщение о создании файла в файл лога.
+                fileCount--; // Уменьшаем счетчик оставшихся файлов.
+            
+        }
+        else { // Иначе (для длины 'length' больше 1) выполняем следующий блок кода (генерация имен папок).
+            std::cout << "Создана папка " << newName << std::endl; // Выводим сообщение о создании папки в консоль.
+            logFile << "Создана папка " << newName << std::endl;   // Записываем сообщение о создании папки в файл лога.
 
-            for (const std::string& subName : subNames) {
-                names.push_back(name + subName);
-            }
-
+            // Рекурсивно генерируем имена для содержимого папки.
+            generateNames(uniqueChars, length - 1, newName, logFile, fileCount);
         }
     }
 }
 
 int main() {
-    setlocale(LC_ALL, "RUS");
-    std::string uniqueChars;
-    int folderCount, fileCount;
+    setlocale(LC_ALL, "RUS"); 
+    std::string uniqueChars;   
+    int folderCount, fileCount; 
 
     std::cout << "Введите уникальные символы для названия папок: ";
-    std::cin >> uniqueChars;
-
-    std::cout << "Введите количество папок: ";
-    std::cin >> folderCount;
-
-    std::cout << "Введите количество файлов: ";
-    std::cin >> fileCount;
+    std::cin >> uniqueChars; 
+    std::cout << "Введите количество папок: "; 
+    std::cin >> folderCount; 
+    std::cout << "Введите количество файлов: "; 
+    std::cin >> fileCount; 
 
     if (uniqueChars.length() == 0 || folderCount < 1 || fileCount < 1) {
-        std::cout << "Ошибка: Некорректный ввод данных." << std::endl;
-        return 1;
+        std::cout << "Ошибка: Некорректный ввод данных." << std::endl; 
+        return 1; 
     }
-
-    std::vector<std::string> folderNames;
-    std::vector<std::string> fileNames;
-
-    generateNames(uniqueChars, folderCount, folderNames);
-    generateNames(uniqueChars, fileCount, fileNames);
 
     std::ofstream logFile("log.txt");
-    int i = 0;
-    for (const std::string& folderName : folderNames) {
-        if (i >= folderCount) break;
-        int j = 0;
-        std::cout << "Создана папка " << folderName << std::endl;
-        logFile << "Создана папка " << folderName << std::endl;
 
-        for (const std::string& fileName : fileNames) {
-            if (j >= fileCount) break;
-            std::cout << "В папке " << folderName << " создан файл " << fileName << std::endl;
-            logFile << "В папке " << folderName << " создан файл " << fileName << std::endl;
-            j++;
-        }
-        i++;
-    }
+    std::string initialName; 
+    generateNames(uniqueChars, folderCount, initialName, logFile, fileCount); 
+
+    logFile.close();
+
+    return 0;
+}
+
 
     logFile.close();
 
